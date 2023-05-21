1ea76c15251c751ec456298ec8ad7a9c62328add
josdem <joseluis.delacruz@gmail.com>
2023-05-21 18:13:10 -0400

    Adding selector by component and css

--- test/e2e/wdio.selectors.js
+++ test/e2e/wdio.selectors.js
@@ -6,6 +6,10 @@ describe('Trello Application', () => {
     })
 
     it('validates expected board', async () => {
-      (await $('h1 data-v-73316130')).isExisting()
+      //Select by component - Never: Too generic no context.
+      //await $('input').setValue('My Board')
+
+      //Select by class - Never: Coupled to syle.
+      await $('.px-2.mt-4.w-full.h-8.bg-white.rounded-sm.border-2').setValue('My Board')
     })
 })
