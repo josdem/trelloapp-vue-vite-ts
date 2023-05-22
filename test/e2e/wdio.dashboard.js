b2acc34b6dee3e1b290cfff00a0ff4af29941470
josdem <joseluis.delacruz@gmail.com>
2023-05-22 16:57:21 -0400

    #1 Adding a todo step

--- test/e2e/wdio.dashboard.js
+++ test/e2e/wdio.dashboard.js
@@ -11,4 +11,9 @@ describe('Trello Application create dashboard', () => {
       await browser.keys(Key.Enter)
       await expect(browser).toHaveUrlContaining('/board/1')
     })
+
+    it('can add a todo on the board', async () => {
+      await $('[data-cy="add-list-input"]').setValue('Add Facebook link')
+      await $('aria/Add a list').click()
+    })
 })
