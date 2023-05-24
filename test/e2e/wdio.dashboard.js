24452f50098f327f86733f9eb3c3a0d07ee26b8f
josdem <joseluis.delacruz@gmail.com>
2023-05-23 20:16:12 -0400

    #1 Validating we have one card in the list

--- test/e2e/wdio.dashboard.js
+++ test/e2e/wdio.dashboard.js
@@ -17,4 +17,11 @@ describe('Trello Application create dashboard', () => {
       await $('aria/Add list').click()
       await expect($('[data-cy="new-card"]')).toBePresent()
     })
+
+    it('can add a card to the list', async () => {
+      await $('[data-cy="new-card"]').click()
+      await $('aria/Enter a title for this card...').setValue('Buy milk')
+      await browser.keys(Key.Enter)
+      await expect($$('div[data-cy="card"]')).toBeElementsArrayOfSize(1)
+    })
 })
